c919c58ed1f00385b0e90b065b162bd72864838f
Marcin Bojara <marcin_5@mail.ru>
2024-02-16 10:34:39 +0100

    fetch events with tanstack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-import Events from './components/Events/Events.jsx';
-import EventDetails from './components/Events/EventDetails.jsx';
-import NewEvent from './components/Events/NewEvent.jsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import EditEvent from './components/Events/EditEvent.jsx';
+import EventDetails from './components/Events/EventDetails.jsx';
+import Events from './components/Events/Events.jsx';
+import NewEvent from './components/Events/NewEvent.jsx';
 
 const router = createBrowserRouter([
   {
@@ -37,8 +34,14 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
